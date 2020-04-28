83d59407063cf853f0560182d1d72c8a30d1b580
V.Galetinko <galetinko@sednor.net>
2020-04-29 01:05:55 +0300

    Initial commit

--- lab5.2/Lab5.2.cpp
+++ lab5.2/Lab5.2.cpp
@@ -1,20 +1,125 @@
-// Lab5.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <locale.h>
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+#define numberOfThreads 10
+#define initSemaphoreCount 3
+#define maxSemaphoreCount 3
+
+HANDLE semaphore;
+
+void showError() {
+	setlocale(LC_ALL, "Russian");
+	cout << "Something went wrong" << endl;
+
+	LPVOID eMessage;
+	DWORD eCode = GetLastError();
+
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL,
+		eCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&eMessage, 0, NULL);
+	char* errorMessage = (char*)eMessage;
+	wprintf(L"ERROR was generated: failed with 0x%x code\n", eCode);
+	wprintf(L"ERROR formatted message: %s\n", errorMessage);
+}
+
+
+DWORD WINAPI ExecutedByThread(LPVOID lpParam)
+{
+	DWORD state, bytesWritten;
+	HANDLE file;
+	clock_t start, finish;
+	char* string = new char[10];
+	bool run = true;
+	const int second = 1000;
+	state = WaitForSingleObject(semaphore, 3);
+
+	while (run == true)
+	{
+		if (state == WAIT_OBJECT_0)
+		{
+			start = clock();
+
+			Sleep(3 * second);
+			cout << "Thread =>" << GetCurrentThreadId() << " is in critical section" << endl;
+
+			//Create file
+			file = CreateFileA("result.txt", GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			finish = clock();
+
+			//count time
+			double duration = finish - start;
+
+			SetFilePointer(file, 0, NULL, FILE_END);
+			//Write a formatted string to character string buffer
+			sprintf(string, "%f", duration);
+
+			//Write result to the file
+			WriteFile(file, string, sizeof(string), &bytesWritten, NULL);
+
+			if (file == INVALID_HANDLE_VALUE)
+			{
+				showError();
+			}
+			run = false;
+		
+			Sleep(3 * second);
+
+			cout << "Thread =>" << GetCurrentThreadId() << " is not in critical section anymore" << endl;
+
+			if (!ReleaseSemaphore(semaphore, 1, NULL))
+			{
+				showError();
+			}
+		}
+	}
+	return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE threads[numberOfThreads];
+	DWORD id;
+
+	semaphore = CreateSemaphore(
+		NULL,			
+		initSemaphoreCount,	  
+		maxSemaphoreCount,	  
+		NULL				
+	);
+
+	if (semaphore == NULL)
+	{
+		showError();
+
+		return 1;
+	}
+
+	//Create threads
+	for (int i = 0; i < numberOfThreads; i++)
+	{
+		threads[i] = CreateThread(NULL, 0, ExecutedByThread, NULL, 0, &id);
+
+		if (threads[i] == NULL)
+		{
+			showError();
+
+			return 1;
+		}
+	}
+
+	WaitForMultipleObjects(numberOfThreads, threads, TRUE, INFINITE);
+
+	for (int i = 0; i < numberOfThreads; i++)
+	{
+		CloseHandle(threads[i]);
+	}
+
+	CloseHandle(semaphore);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- lab5.3/Lab5.3.cpp
+++ lab5.3/Lab5.3.cpp
@@ -1,20 +1,167 @@
-// Lab5.3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+#define CRITICAL_SECTION_MODE 0
+#define NOT_CRITICAL_SECTION_MODE 1
+
+#define numberOfThreads 3
+#define amountOfNumbers 10101110 // 10 101 110
+
+int arr[amountOfNumbers];
+CRITICAL_SECTION criricalSection;
+
+void getSum()
+{
+	int sum = 0;
+	for (int i = 0; i < amountOfNumbers; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Sum => " << sum << endl;
+}
+
+void getAverage()
+{
+	double avg = 0;
+	for (int i = 0; i < amountOfNumbers; i++)
+	{
+		avg += arr[i];
+	}
+	cout << "Avarage => " << avg / amountOfNumbers << endl;
+}
+
+void getMaximum()
+{
+	int max = 0;
+	for (int i = 0; i < amountOfNumbers; i++)
+	{
+		if (max < arr[i])
+			max = arr[i];
+	}
+	cout << "Max => " << max << endl;
+}
+
+void generate()
+{
+	for (int i = 0; i < amountOfNumbers; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+DWORD WINAPI findSum(LPVOID mode)
+{
+	if (NOT_CRITICAL_SECTION_MODE)
+	{
+		getSum();
+	}
+	else {
+		if (!InitializeCriticalSectionAndSpinCount(&criricalSection, 0x00000400))
+		{
+			return 0;
+		}
+		EnterCriticalSection(&criricalSection);
+		getSum();
+		LeaveCriticalSection(&criricalSection);
+	}
+
+	return 0;
+}
+
+DWORD WINAPI findAverage(LPVOID mode)
+{
+	if (NOT_CRITICAL_SECTION_MODE)
+	{
+		getAverage();
+	}
+	else {
+		if (!InitializeCriticalSectionAndSpinCount(&criricalSection, 0x00000400))
+		{
+			return 0;
+		}
+		EnterCriticalSection(&criricalSection);
+		getAverage();
+		LeaveCriticalSection(&criricalSection);
+	}
+
+	return 0;
+}
+
+DWORD WINAPI findMaximum(LPVOID mode)
+{
+
+	if (NOT_CRITICAL_SECTION_MODE)
+	{
+		getMaximum();
+	}
+	else {
+		if (!InitializeCriticalSectionAndSpinCount(&criricalSection, 0x00000400))
+		{
+			return 0;
+		}
+		EnterCriticalSection(&criricalSection);
+		getMaximum();
+		LeaveCriticalSection(&criricalSection);
+	}
+
+	return 0;
+}
+
+void DeleteCriticalSection()
+{
+	DeleteCriticalSection(&criricalSection);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE hThread[numberOfThreads];
+	DWORD ThreadId;
+	clock_t start, finish;
+
+	generate();
+
+	cout << "First mode = > use critical section" << endl;
+	start = clock();
+	//Create threads & execute
+	hThread[0] = CreateThread(NULL, 0, findSum, (LPVOID)CRITICAL_SECTION_MODE, 0, &ThreadId);
+	hThread[1] = CreateThread(NULL, 0, findAverage, (LPVOID)CRITICAL_SECTION_MODE, 0, &ThreadId);
+	hThread[2] = CreateThread(NULL, 0, findMaximum, (LPVOID)CRITICAL_SECTION_MODE, 0, &ThreadId);
+	WaitForMultipleObjects(numberOfThreads, hThread, TRUE, INFINITE);
+
+	finish = clock();
+	double duration = finish - start;
+	cout << "Time: " << duration << endl;
+
+	DeleteCriticalSection();
+
+	for (int i = 0; i < numberOfThreads; i++)
+	{
+		CloseHandle(hThread[i]);
+	}
+
+	cout << "Second mode = > not use critical section" << endl;
+	start = clock();
+
+	hThread[0] = CreateThread(NULL, 0, findSum, (LPVOID)NOT_CRITICAL_SECTION_MODE, 0, &ThreadId);
+	hThread[1] = CreateThread(NULL, 0, findAverage, (LPVOID)NOT_CRITICAL_SECTION_MODE, 0, &ThreadId);
+	hThread[2] = CreateThread(NULL, 0, findMaximum, (LPVOID)NOT_CRITICAL_SECTION_MODE, 0, &ThreadId);
+	WaitForMultipleObjects(numberOfThreads, hThread, TRUE, INFINITE);
+
+	finish = clock();
+	duration = finish - start;
+	cout << "Time: " << duration << endl;
+
+	DeleteCriticalSection();
+
+	for (int i = 0; i < numberOfThreads; i++)
+	{
+		CloseHandle(hThread[i]);
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
